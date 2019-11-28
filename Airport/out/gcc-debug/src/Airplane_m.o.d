12d21edc614c048bccb7b9334ac012ec766556a8
luigi94 <luigi.treccozzi@libero.it>
2019-11-28 17:38:04 +0100

    some updates

--- Airport/out/gcc-debug/src/Airplane_m.o.d
+++
@@ -1,6 +0,0 @@
-../out/gcc-debug/src/Airplane_m.o: Airplane_m.cc Airplane_m.h \
- /home/luigi/Downloads/omnetpp-5.5.1/include/omnetpp.h
-
-Airplane_m.h:
-
-/home/luigi/Downloads/omnetpp-5.5.1/include/omnetpp.h:
--- Airport/out/gcc-debug/src/ControlTower.o.d
+++
@@ -1,8 +0,0 @@
-../out/gcc-debug/src/ControlTower.o: ControlTower.cc ControlTower.h \
- /home/luigi/Downloads/omnetpp-5.5.1/include/omnetpp.h Airplane_m.h
-
-ControlTower.h:
-
-/home/luigi/Downloads/omnetpp-5.5.1/include/omnetpp.h:
-
-Airplane_m.h:
